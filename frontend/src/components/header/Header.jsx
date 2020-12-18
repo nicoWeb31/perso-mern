7f8f2dc0eeddae3dcd1a4d0c9ff0283394945c64
nicoWeb31 <nico.riot@free.fr>
2020-12-18 19:29:39 +0100

    titre header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import './header.style.css';
+import React from "react";
+import "./header.style.css";
+import logo from "../../img/logo.png";
 
 const Header = () => {
     return (
-        <header className='header'>
-            header
-            
-        </header>
-    )
-}
+        <header className="header">
+            <div className="logo-box">
+                <img src={logo} alt="logo" className="logo" />
+            </div>
+            <div className='text-box'>
+                <h1 className="heading-primary">
 
-export default Header
+                    <span className="heading-primary-main">
+                        Riot Nicolas
+                    </span>
+                    <span className="heading-primary-secondary">
+                        Création de contenu numérique
+                    </span>
+                </h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
