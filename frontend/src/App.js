6711e8d9adc76f094c3d5241ea6867476d74af1a
nicoWeb31 <nico.riot@free.fr>
2020-12-22 11:54:08 +0100

    about html css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
-
-import './App.css';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import About from "./pages/about/About";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+
+                <main className="main">
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/about" component={About} />
+                </main>
+
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/about/About.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./about.style.scss";
+
+const About = () => {
+    return (
+        <div className="about">
+            <div className="u-center-text u-margin-bottom-8">
+                <h2 className="about--title">Développeur web Mern Stack</h2>
+            </div>
+            <div className="row">
+                <div className="col-md-6 col-12">
+                    <h3 className="heading-tertiary">
+                        Pour des projets clés en main!
+                    </h3>
+                    <p className="paragraphe">
+                        En résumé, votre page À Propos accueillera l’histoire de
+                        la création de votre marque, et elle présentera les
+                        points forts de votre entreprise. Plus important encore,
+                        elle servira de page de vente apportant des réponses à
+                        la question la plus importante que se posent les
+                        nouveaux clients (outre la question « Pourquoi
+                        devrais-je acheter les produits de cette marque ? »), à
+                        savoir : Pourquoi devrais-je faire confiance à cette
+                        marque ou à ce marchand ?
+                    </p>
+
+                    <h3 className="heading-tertiary">Pour des Missions Pro !</h3>
+                    <p className="paragraph">
+                        En résumé, votre page À Propos accueillera l’histoire de
+                        la création de votre marque, et elle présentera les
+                        points forts de votre entreprise. Plus important encore,
+                        elle servira de page de vente apportant des réponses à
+                        la question la plus importante que se posent les
+                        nouveaux clients (outre la question « Pourquoi
+                        devrais-je acheter les produits de cette marque ? »), à
+                        savoir : Pourquoi devrais-je faire confiance à cette
+                        marque ou à ce marchand ?
+                    </p>
+                </div>
+                <div className="col-md-6 col-12">tata</div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
