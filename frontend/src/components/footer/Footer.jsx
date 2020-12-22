fa47502c811644ee05f30da1f7c6df17c7685aca
nicoWeb31 <nico.riot@free.fr>
2020-12-22 21:18:08 +0100

    retour 13 commit ..

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer>
-            footer
-        </footer>
-    )
-}
+        <div className="footer">
+            <div className="footer__logo-box">
+                <i
+                    class="fas fa-laptop-code fa-5x"
+                    className="footer__logo"
+                ></i>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    <div className="footer__nav">
+                        <ul className="footer__list">
+                            <li className="footer__item">
+                                <Link to="/" className="footer-link">
+                                    Accueil
+                                </Link>
+                            </li>
+                            <li className="footer__item">
+                                <Link to="/about" className="footer-link">
+                                    A Propos
+                                </Link>
+                            </li>
+                            <li className="footer__item">
+                                <Link to="/contact" className="footer-link">
+                                    Contact
+                                </Link>
+                            </li>
+                            <li className="footer__item">
+                                <Link to="/service" className="footer-link">
+                                    Service
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <p className="footer__copyright">
+                        Copyright &copy; by Riot Nicolas 2020
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,38 +1,89 @@
 import React from "react";
 import "./homepage.style.scss";
-import me from '../../img/me.jpg';
+import me from "../../img/homeN/me.jpg";
+import code from "../../img/homeN/code.jpg";
+
+import bgVideo from "../../img/homeN/Laptop.mp4";
 
 const HomePage = () => {
     return (
         <div className="home">
+            <div className="bg-video">
+                <video className="bg-video__content" autoPlay muted loop>
+                    <source src={bgVideo} type="video/mp4" />
+                    your Browser is not supported !
+                </video>
+            </div>
             <div className="u-center-text ">
-                <h2 className="heading-secondary">
+                <h2 className="heading-secondary ">
                     Developper Web/Web Mobile Mern Stack
                 </h2>
             </div>
             <div className="row">
+                <div className="col-md-6 col-12">
+                    <div className="presentation ">
+                        <figure className="presentation__shape">
+                            <img
+                                src={me}
+                                alt="riot nicolas"
+                                className="presentation__img"
+                            />
+                        </figure>
+                        <div className="presentation__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">
+                                Qui suis-je ?
+                            </h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Facilis alias, quos quis
+                                soluta dolorem nesciunt fugiat aut rem
+                                voluptatibus exercitationem eaque sapiente vitae
+                                aliquid, ad officiis accusantium autem
+                                consequuntur animi! Omnis voluptatum vero,
+                                sapiente est libero deleniti eaque quos
+                                reprehenderit nostrum deserunt quaerat ducimus
+                                minima recusandae nesciunt assumenda corporis
+                                rerum laudantium ea quo consequuntur error earum
+                                quae optio. Sed, dolorem? Possimus, cum illum
+                                vel eum fugit molestiae autem eaque commodi
+                                nulla culpa rerum vitae ducimus amet veniam non
+                                earum? Perferendis eum quaerat error porro
+                                libero sunt illum architecto molestias! Laborum!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-12">
                 <div className="presentation ">
-                    <figure className="presentation__shape">
-                        <img src={me} alt="riot nicolas" className="presentation__img"/>
-                    </figure>
-                    <div className="presentation__text">
-                        <h3 className="heading-tertiary u-margin-bottom-small">Qui suis-je ?</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Facilis alias, quos quis soluta dolorem
-                            nesciunt fugiat aut rem voluptatibus exercitationem
-                            eaque sapiente vitae aliquid, ad officiis
-                            accusantium autem consequuntur animi! Omnis
-                            voluptatum vero, sapiente est libero deleniti eaque
-                            quos reprehenderit nostrum deserunt quaerat ducimus
-                            minima recusandae nesciunt assumenda corporis rerum
-                            laudantium ea quo consequuntur error earum quae
-                            optio. Sed, dolorem? Possimus, cum illum vel eum
-                            fugit molestiae autem eaque commodi nulla culpa
-                            rerum vitae ducimus amet veniam non earum?
-                            Perferendis eum quaerat error porro libero sunt
-                            illum architecto molestias! Laborum!
-                        </p>
+                        <figure className="presentation__shape">
+                            <img
+                                src={code}
+                                alt="code"
+                                className="presentation__img"
+                            />
+                        </figure>
+                        <div className="presentation__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">
+                                Ce que je propose ?
+                            </h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Facilis alias, quos quis
+                                soluta dolorem nesciunt fugiat aut rem
+                                voluptatibus exercitationem eaque sapiente vitae
+                                aliquid, ad officiis accusantium autem
+                                consequuntur animi! Omnis voluptatum vero,
+                                sapiente est libero deleniti eaque quos
+                                reprehenderit nostrum deserunt quaerat ducimus
+                                minima recusandae nesciunt assumenda corporis
+                                rerum laudantium ea quo consequuntur error earum
+                                quae optio. Sed, dolorem? Possimus, cum illum
+                                vel eum fugit molestiae autem eaque commodi
+                                nulla culpa rerum vitae ducimus amet veniam non
+                                earum? Perferendis eum quaerat error porro
+                                libero sunt illum architecto molestias! Laborum!
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
