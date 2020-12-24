05eecaac1cba44e78d03f91c5c92c63faea7e972
nicoWeb31 <nico.riot@free.fr>
2020-12-24 15:13:08 +0100

    test server de prod

--- back/app.js
+++ back/app.js
@@ -1,30 +1,43 @@
 //express server
-import express from 'express';
+import express from "express";
+import path from "path";
 const app = express();
-import morgan from 'morgan';
+import morgan from "morgan";
+
 // import path from 'path';
 
-
 //middleware server
-app.use((req,res,next) => {
-    console.log('welcome in first middleware 😻 ');
+app.use((req, res, next) => {
+    console.log("welcome in first middleware 😻 ", process.env.NODE_ENV);
+
     next();
-})
+});
 
 // const __dirname = path.resolve();
 // app.use('/images',express.static(__dirname + '/images/'))
 
-//MORGAN 
-if(process.env.NODE_ENV !== 'production') {
+//MORGAN
+if (process.env.NODE_ENV !== "production") {
     app.use(morgan("dev"));
 }
 
-
 //router server
 
+//production environment
+const __dirname = path.resolve();
+console.log("🚀 ~ file: app.js ~ line 28 ~ __dirname", __dirname)
+
+if (process.env.NODE_ENV === "production") {
+    console.log("production mode");
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        console.log("dev mode");
+        res.send("Api is runnig.....");
+    });
+}
+
 export default app;
-
-
-
-
-
--- back/server.js
+++ back/server.js
@@ -1,19 +1,18 @@
-import app from './app.js';
-import connectDB from './config/bd.js'
-import dotenv from 'dotenv';
-import colors from 'colors';//colorConsole
-dotenv.config();
+import app from "./app.js";
+import connectDB from "./config/bd.js";
 
+import dotenv from "dotenv";
+import colors from "colors"; //colorConsole
+dotenv.config();
 
 //____________________________MONGODB CONNECT_________________________________
 connectDB();
 
-
-
-
-
 //server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server run in ${process.env.NODE_ENV} on port ${PORT} 🙂 ...`.yellow.bold);
-})
+    console.log(
+        `Server run in ${process.env.NODE_ENV} on port ${PORT} 🙂 ...`.yellow
+            .bold
+    );
+});
