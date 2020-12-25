d43326ccf4d4bdb648f6d8a485f5fb341647ab96
nicoWeb31 <nico.riot@free.fr>
2020-12-25 18:06:12 +0100

    construction du formulaire

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -9,7 +9,27 @@ const Contact = () => {
                 <h2 className="heading-secondary">vous pouvez me contacter </h2>
             </div>
             <div className="formulaire">
-                
+                <div className="formulaire__form">
+                    <h4>Laisser moi un message</h4>
+                    <form action="">
+                        <div className="form__group">
+                            <label htmlFor="nom">Votre nom</label>
+                            <input type="text" className="form__input" placeholder="votre nom" id="nom" required/>
+                        </div>
+                        <div className="form__group">
+                            <label htmlFor="email">Votre mail</label>
+                            <input type="email" className="form__input" placeholder="votre nom" id="email" required/>
+                        </div>
+                        <div className="form__group">
+                            <label htmlFor="massage">Message</label>
+                            {/* <input type="email" className="form__input" placeholder="votre nom" id="email" required/> 
+                            */}
+                            <textarea name="" id="message" cols="30" rows="10"></textarea>
+                        </div>
+                    </form>
+
+                </div>
+
             </div>
         </div>
     )
