aebe8e94f15e995bb7010b864689cc1a8f9972ca
nicoWeb31 <nico.riot@free.fr>
2020-12-28 19:49:36 +0100

    css formulaire ok

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -13,18 +13,18 @@ const Contact = () => {
                     <h4 className="heading-tertiary">Laisser moi un message</h4>
                     <form action="" className="form">
                         <div className="form__group">
-                            <label htmlFor="nom" className="form__label">Votre nom</label>
                             <input type="text" className="form__input" placeholder="votre nom" id="nom" required/>
+                            <label htmlFor="nom" className="form__label">Votre nom</label>
                         </div>
                         <div className="form__group">
+                            <input type="email" className="form__input" placeholder="votre mail" id="email" required/>
                             <label htmlFor="email" className="form__label">Votre mail</label>
-                            <input type="email" className="form__input" placeholder="votre nom" id="email" required/>
                         </div>
                         <div className="form__group">
+                            <textarea name="" id="message" cols="30" rows="10" className="form__input" placeholder="votre message"></textarea>
                             <label htmlFor="massage" className="form__label">Message</label>
                             {/* <input type="email" className="form__input" placeholder="votre nom" id="email" required/> 
                             */}
-                            <textarea name="" id="message" cols="30" rows="10" className="form__input"></textarea>
                         </div>
                     </form>
 
