fa0f258e69727fddd54d6b84a57f7d54497be8df
nicoWeb31 <nico.riot@free.fr>
2021-01-02 16:18:47 +0100

    component alert, action create

---
+++ frontend/src/components/alert/Alert.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alert = (variant, children) => {
+    return (
+        <div className={`${variant} alert`} >
+            
+        </div>
+    )
+}
+
+export default Alert
---
+++ frontend/src/redux/actions/massageAction.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import {
+    CREATE_MESSAGE_SUCCESS,
+    CREATE_MESSAGE_FAIL,
+    CREATE_MESSAGE_REQUEST,
+} from "../type/messageType";
+
+export const createMessage = (message) => async (dispatch) => {
+    try {
+        dispatch({ type: CREATE_MESSAGE_REQUEST });
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        const { data } = axios.post(
+            "http://localhost:3005/api/v1/",
+            message,
+            config
+        );
+        dispatch({ type: CREATE_MESSAGE_SUCCESS, payload: data.message });
+    } catch (error) {
+        dispatch({ type: CREATE_MESSAGE_FAIL,payload:error.response && error.data.message ? error.data.message : error.message});
+    }
+};
