9b57f58b571e0b4614adcd5598ed951d7b1f1f64
nicoWeb31 <nico.riot@free.fr>
2021-01-03 19:02:31 +0100

    refacto render ok

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -1,45 +1,85 @@
 import React, { useState, useEffect } from "react";
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import Spinner from "../../components/spinner/Spinner";
-import Alert from '../../components/alert/Alert';
-import { createMessage } from '../../redux/actions/massageAction';
-import {Field, reduxForm} from 'redux-form';
+import Alert from "../../components/alert/Alert";
+import { createMessage } from "../../redux/actions/massageAction";
+import { Field, reduxForm } from "redux-form";
 
 import "./contact.style.scss";
 
-const Contact = ({history}) => {
-
-
-    const [name,setName] = useState('');
-    const [email,setEmail] = useState('');
-    const [message,setMessage] = useState('');
-    const [successMess, setSuccessMess] = useState("Votre message a été envoyer avec success!  j'y repondrai des que possible ! merci");
-    
-
+const Contact = ({ history }) => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [successMess, setSuccessMess] = useState(
+        "Votre message a été envoyer avec success!  j'y repondrai des que possible ! merci"
+    );
 
     const dispatch = useDispatch();
 
-    const newMessage = useSelector(state=>state.messages);
-    const {loading, error,success} = newMessage;
+    const newMessage = useSelector((state) => state.messages);
+    const { loading, error, success } = newMessage;
 
-    useEffect(()=>{
-        if(success){
-            history.push('/contact')
+    useEffect(() => {
+        if (success) {
+            history.push("/contact");
         }
+    }, [history, success]);
 
-    },[history,success]);
+    //_________________________________render input______________________________________
 
+    const renderInput = (formProps) => {
+        return (
+            <div className="form__group">
+                <input
+                    {...formProps.input}
+                    // type="text"
+                    className="form__input"
+                    placeholder="votre nom"
+                    // value={formProps.input.value}
+                    // name="name"
+                    // onChange={formProps.input.onChange}
+                    // id="nom"
+                    // required
+                />
+                <label htmlFor="nom" className="form__label">
+                    {formProps.label}
+                </label>
+            </div>
+        );
+    };
+
+    const renderTextarea = (formProps) => {
+        return(
+        <div className="form__group">
+            <textarea
+                name="message"
+                value={formProps.input.value}
+                onChange={formProps.input.onChange}
+                id="message"
+                cols="30"u
+                rows="10"
+                className="form__input"
+                placeholder="votre message"
+            ></textarea>
+            <label htmlFor="massage" className="form__label">
+                {formProps.label}
+            </label>
+        </div>
+
+        )
+    };
 
     //_________________________________fonction______________________________________
-    const onHandleSubmit =(e)=>{
+    const onHandleSubmit = (e) => {
         e.preventDefault();
-        const data = {name,email,message};
-        console.log(data)
-        dispatch(createMessage(data))
-        setName('');
-        setMessage('');
-        setEmail('');
-    }
+        const data = { name, email, message };
+        console.log(data);
+        dispatch(createMessage(data));
+        setName("");
+        setMessage("");
+        setEmail("");
+    };
     return (
         <div className="contact">
             <div className="u-center-text u-margin-bottom-big">
@@ -47,63 +87,27 @@ const Contact = ({history}) => {
             </div>
             <div className="spin">
                 {loading && <Spinner message="" />}
-                {error && <Alert variant='danger'>{error}</Alert>} 
-                {success && <Alert variant="success">{successMess}</Alert>} 
-
+                {error && <Alert variant="danger">{error}</Alert>}
+                {success && <Alert variant="success">{successMess}</Alert>}
             </div>
 
             <div className="formulaire">
                 <div className="formulaire__form">
                     <h4>Laisser moi un message</h4>
                     <form onSubmit={onHandleSubmit} className="form">
-                        <div className="form__group">
-                            <input
-                                
-                                type="text"
-                                className="form__input"
-                                placeholder="votre nom"
-                                value={name}
-                                name="name"
-                                onChange={e=>setName(e.target.value)}
-                                id="nom"
-                                required
-                            />
-                            <label htmlFor="nom" className="form__label">
-                                Votre nom
-                            </label>
-                        </div>
-                        <div className="form__group">
-                            <input
-                                type="email"
-                                className="form__input"
-                                placeholder="votre mail"
-                                id="email"
-                                name="email"
-                                value={email}
-                                onChange={e=>setEmail(e.target.value)}
-                                required
-                            />
-                            <label htmlFor="email" className="form__label">
-                                Votre mail
-                            </label>
-                        </div>
-                        <div className="form__group">
-                            <textarea
-                                name="message"
-                                value={message}
-                                onChange={(e)=>setMessage(e.target.value)}
-                                id="message"
-                                cols="30"
-                                rows="10"
-                                className="form__input"
-                                placeholder="votre message"
-                            ></textarea>
-                            <label htmlFor="massage" className="form__label">
-                                Message
-                            </label>
-                            {/* <input type="email" className="form__input" placeholder="votre nom" id="email" required/>
-                             */}
-                        </div>
+                        <Field
+                            name="name"
+                            component={renderInput}
+                            label="Votre nom ?"
+                        />
+
+                        <Field
+                            name="email"
+                            component={renderInput}
+                            label="Votre mail ?"
+                        />
+                        
+                        <Field name="message" component={renderTextarea} label="Votre Message ?"/>
 
                         <button className="btn-submit" type="submit">
                             <i className="fas fa-arrow-right"></i>
@@ -116,10 +120,6 @@ const Contact = ({history}) => {
     );
 };
 
-
-
-
-
 export default reduxForm({
-    form: 'messageForm'
+    form: "messageForm",
 })(Contact);
